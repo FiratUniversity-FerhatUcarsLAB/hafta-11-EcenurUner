253f5389a9b59612baae541bbba9ea4387c15fed
EcenurUner <250541078@firat.edu.tr>
2025-12-03 15:39:27 +0300

    Enhance comments on method usage and return values
    Added comments explaining the behavior of methods.

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -11,10 +11,14 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
-        getNumber();
+        getNumber(); // 1.Cevap: Program hata vermez, sorunsuz derlenir.
+        // Ancak çoğu IDE/Derleyici "Dönen değer kullanılmadı" şeklinde bir uyarı verir.
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+        // 2.Cevap: DERLEME HATASI verir.(Compile-time error)
+        //Hata mesajı: 'void' type not allowed here.
+        // sayHello() metdu bir değer döndürmez. Toplama gibi ifadeler değer gerektirdiğinden, değeri olmayan bir metotla toplama yapılamaz.
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
